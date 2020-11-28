cd28ad8bfa5ff5506e536c343e6316f96ed3b923
washinson <sanya112001@yandex.ru>
2020-11-28 22:49:24 +0300

    Initial Commit

---
+++ CommitsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CommitsApp
+//
+//  Created by alex on 28.11.2020.
+//  Copyright © 2020 washinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
